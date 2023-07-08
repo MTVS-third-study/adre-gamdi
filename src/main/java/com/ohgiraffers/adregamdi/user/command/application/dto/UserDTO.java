65b7ab58f7d1ecab7c3db69cce74ac0d84b717fc
manje cho <whakswp123@naver.com>
2023-07-09 03:26:23 +0900

    feat: 유저 dto, entity 생성

--- src/main/java/com/ohgiraffers/adregamdi/user/command/application/dto/UserDTO.java
+++ src/main/java/com/ohgiraffers/adregamdi/user/command/application/dto/UserDTO.java
@@ -1,4 +1,15 @@
 package com.ohgiraffers.adregamdi.user.command.application.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class UserDTO {
+    private String id;
+    private String nickname;
+    private String email;
+    private String gender;
 }
--- src/main/java/com/ohgiraffers/adregamdi/user/command/domain/aggregate/entity/User.java
+++ src/main/java/com/ohgiraffers/adregamdi/user/command/domain/aggregate/entity/User.java
@@ -1,4 +1,32 @@
 package com.ohgiraffers.adregamdi.user.command.domain.aggregate.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    @Column
+    private String nickname;
+    @Column
+    private String email;
+    @Column
+    private String gender;
+
+    public User update(String id, String nickname, String email, String gender) {
+        this.id = id;
+        this.nickname = nickname;
+        this.email = email;
+        this.gender = gender;
+        return this;
+    }
+
 }
