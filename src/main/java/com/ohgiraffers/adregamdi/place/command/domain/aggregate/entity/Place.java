104c996bfae376c60db8fb230b150dba84452fd7
anichan <qufekf97@gmail.com>
2023-07-09 17:21:07 +0900

    장소 entity 제작

---
+++ src/main/java/com/ohgiraffers/adregamdi/place/command/domain/aggregate/entity/Place.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.adregamdi.place.command.domain.aggregate.entity;
+
+public class Place {
+
+    private int placeID;
+    private String placeName;
+    private double latitude;
+    private double longitude;
+    private String phoneNumber;
+    private String categoryCode;
+    private String categoryName;
+    private String categoryGroupName;
+    private String placeUrl;
+    private String placeAddress;
+    private String roadPlaceAddress;
+}
