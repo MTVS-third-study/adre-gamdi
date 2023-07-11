d9ae036e40f2fb19673c86df3fadbb6067ece6cf
manje cho <whakswp123@naver.com>
2023-07-11 22:58:17 +0900

    feat: 조회 메소드 삭제

--- src/main/java/com/ohgiraffers/adregamdi/user/command/domain/repository/UserRepository.java
+++ src/main/java/com/ohgiraffers/adregamdi/user/command/domain/repository/UserRepository.java
@@ -1,17 +1,12 @@
 package com.ohgiraffers.adregamdi.user.command.domain.repository;
 
 import com.ohgiraffers.adregamdi.user.command.application.dto.UserDTO;
-import com.ohgiraffers.adregamdi.user.command.domain.aggregate.entity.User;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository {
     int insertUser(UserDTO userInfo);
-
-    User selectUserOne(int id);
-
-    void selectUserList();
-
+    
     void updateUser();
 
     void deleteUser();
--- src/main/java/com/ohgiraffers/adregamdi/user/command/infrastructure/repository/UserInfraRepository.java
+++ src/main/java/com/ohgiraffers/adregamdi/user/command/infrastructure/repository/UserInfraRepository.java
@@ -1,7 +1,6 @@
 package com.ohgiraffers.adregamdi.user.command.infrastructure.repository;
 
 import com.ohgiraffers.adregamdi.user.command.application.dto.UserDTO;
-import com.ohgiraffers.adregamdi.user.command.domain.aggregate.entity.User;
 import com.ohgiraffers.adregamdi.user.command.domain.repository.UserRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,19 +8,8 @@ import org.springframework.stereotype.Repository;
 public class UserInfraRepository implements UserRepository {
     @Override
     public int insertUser(UserDTO userInfo) {
-
+        //
         return 1;
-    }
-
-    @Override
-    public User selectUserOne(int id) {
-
-        return null;
-    }
-
-    @Override
-    public void selectUserList() {
-
     }
 
     @Override
