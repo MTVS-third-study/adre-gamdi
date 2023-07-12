3c77e67df6a8c2a7cb6cff91cf336936e127acd5
anichan <qufekf97@gmail.com>
2023-07-12 16:22:30 +0900

    장소 엔티티 재수정

--- src/main/java/com/ohgiraffers/adregamdi/place/command/domain/aggregate/entity/Place.java
+++ src/main/java/com/ohgiraffers/adregamdi/place/command/domain/aggregate/entity/Place.java
@@ -1,16 +1,16 @@
 package com.ohgiraffers.adregamdi.place.command.domain.aggregate.entity;
 
-public class Place {
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
-    private int placeID;
+
+public class Place {
+    private int placeNumber;
     private String placeName;
     private double latitude;
     private double longitude;
     private String phoneNumber;
     private String categoryCode;
-    private String categoryName;
-    private String categoryGroupName;
-    private String placeUrl;
     private String placeAddress;
     private String roadPlaceAddress;
 }
