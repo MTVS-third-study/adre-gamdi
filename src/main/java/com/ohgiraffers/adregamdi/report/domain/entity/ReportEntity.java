91008e4588ac24edfa55d020a968c9454da26ce6
hyojin lee <hodin030@gmail.com>
2023-07-12 16:28:42 +0900

    fix 리뷰, 신고 기능
    엔티티 수정

--- src/main/java/com/ohgiraffers/adregamdi/report/domain/entity/ReportEntity.java
+++ src/main/java/com/ohgiraffers/adregamdi/report/domain/entity/ReportEntity.java
@@ -1,72 +1,19 @@
 package com.ohgiraffers.adregamdi.report.domain.entity;
 
+import lombok.*;
+
 import java.util.Date;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class ReportEntity {
-
     private int reportNo;
-    private int reporterNo;
-    private int respondentNo;
     private int reportCategoryCode;
     private String reportContents;
     private Date reportedDate;
-
-    public ReportEntity(int reportNo, int reporterNo, int respondentNo, int reportCategoryCode, String reportContents, Date reportedDate) {
-        this.reportNo = reportNo;
-        this.reporterNo = reporterNo;
-        this.respondentNo = respondentNo;
-        this.reportCategoryCode = reportCategoryCode;
-        this.reportContents = reportContents;
-        this.reportedDate = reportedDate;
-    }
-
-    public int getReportNo() {
-        return reportNo;
-    }
-
-    public void setReportNo(int reportNo) {
-        this.reportNo = reportNo;
-    }
-
-    public int getReporterNo() {
-        return reporterNo;
-    }
-
-    public void setReporterNo(int reporterNo) {
-        this.reporterNo = reporterNo;
-    }
-
-    public int getRespondentNo() {
-        return respondentNo;
-    }
-
-    public void setRespondentNo(int respondentNo) {
-        this.respondentNo = respondentNo;
-    }
-
-    public int getreportCategoryCode() {
-        return reportCategoryCode;
-    }
-
-    public void setreportCategoryCode(int reportCategoryCode) {
-        this.reportCategoryCode = reportCategoryCode;
-    }
-
-    public String getReportContents() {
-        return reportContents;
-    }
-
-    public void setReportContents(String reportContents) {
-        this.reportContents = reportContents;
-    }
-
-    public Date getReportedDate() {
-        return reportedDate;
-    }
-
-    public void setReportedDate(Date reportedDate) {
-        this.reportedDate = reportedDate;
-    }
-
-
+    private reporterNo reporterNo;
+    private respondentNo respondentNo;
 }
--- src/main/java/com/ohgiraffers/adregamdi/review/domain/entity/ReviewEntity.java
+++ src/main/java/com/ohgiraffers/adregamdi/review/domain/entity/ReviewEntity.java
@@ -1,7 +1,6 @@
 package com.ohgiraffers.adregamdi.review.domain.entity;
 
 import lombok.*;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -11,23 +10,15 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-@Entity
-@Table(name = "REVIEW_TBL")
 public class ReviewEntity {
 
-    @Id
     private int reviewNo;
-    @Column
     private int likeNum;
-    @Column
     private double starPoint;
-    @Column
     private String reviewImage;
-    @Column
     private String reviewContents;
-    @Column
     private boolean reportStatus;
-    private int userNo;
-    private int placeNo;
     private Date regDate;
+    private reviewWriterNo reviewWriterNo;
+    private reviewPlaceNo reviewPlaceNo;
 }
