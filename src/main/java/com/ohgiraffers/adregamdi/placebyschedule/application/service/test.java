f2332ba7fb19286c8e2d331a947b71add0d481a9
wanny <kko6369@naver.com>
2023-07-12 16:46:36 +0900

    feat: 스케줄과 스케줄 장소 엔티티 생성

---
+++ src/main/java/com/ohgiraffers/adregamdi/placebyschedule/application/service/test.java
@@ -0,0 +1,4 @@
+package com.ohgiraffers.adregamdi.placebyschedule.application.service;
+
+public class test {
+}
--- src/main/java/com/ohgiraffers/adregamdi/schedule/command/domain/aggregate/entity/test.java
+++
@@ -1,4 +0,0 @@
-package com.ohgiraffers.adregamdi.schedule.command.domain.aggregate.entity;
-
-public class test {
-}
