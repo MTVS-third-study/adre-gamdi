40da406ba9f6a6f8fa262521c970bc64e712f82d
UncleSubak <kko6369@naver.com>
2023-07-16 00:29:30 +0900

    feat: Entity 추가

--- src/main/java/com/ohgiraffers/adregamdi/place/command/domain/aggregate/vo/Address.java
+++ src/main/java/com/ohgiraffers/adregamdi/place/command/domain/aggregate/vo/Address.java
@@ -1,8 +1,14 @@
 package com.ohgiraffers.adregamdi.place.command.domain.aggregate.vo;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Address {
 
+    @Column(name = "PLACE_ADDRESS")
     private String placeAddress;
+    @Column(name = "ROAD_PLACE_ADDRESS")
     private String roadPlaceAddress;
 
 }
--- src/main/java/com/ohgiraffers/adregamdi/place/command/domain/aggregate/vo/Coordinate.java
+++ src/main/java/com/ohgiraffers/adregamdi/place/command/domain/aggregate/vo/Coordinate.java
@@ -1,7 +1,13 @@
 package com.ohgiraffers.adregamdi.place.command.domain.aggregate.vo;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Coordinate {
 
+    @Column(name = "LATITUDE")
     private double latitude;
+    @Column(name = "LONGITUDE")
     private double longitude;
 }
