51727aecdc1a33aaf5616f65df96fe5f988dd81c
manje cho <whakswp123@naver.com>
2023-07-18 02:13:10 +0900

    feat: 카카오 로그인 버그 수정 (#52)

--- src/main/java/com/ohgiraffers/adregamdi/user/command/application/dto/UserDTO.java
+++ src/main/java/com/ohgiraffers/adregamdi/user/command/application/dto/UserDTO.java
@@ -1,6 +1,5 @@
 package com.ohgiraffers.adregamdi.user.command.application.dto;
 
-import com.ohgiraffers.adregamdi.user.command.domain.aggregate.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,22 +22,22 @@ public class UserDTO {
     private int grade;
     private boolean blacklist_status;
 
-    public static UserDTO of(User user){
-        UserDTO userDTO=new UserDTO();
-        userDTO.userNo=user.getUserNo();
-        userDTO.id=user.getId();
-        userDTO.kakaoNickName=user.getKakaoNickName();
-        userDTO.serviceNickName=user.getServiceNickName();
-        userDTO.email=user.getEmail();
-        userDTO.age=user.getAge();
-        userDTO.gender=user.getGender();
-        userDTO.report_count=user.getReport_count();
-        userDTO.review_count=user.getReview_count();
-        userDTO.grade=user.getGrade();
-        userDTO.blacklist_status=user.isBlacklist_status();
-        userDTO.access_Token=user.getAccess_Token();
-        userDTO.refresh_Token=user.getRefresh_Token();
-        return userDTO;
-
-    }
+//    public static UserDTO of(User user){
+//        UserDTO userDTO=new UserDTO();
+//        userDTO.userNo=user.getUserNo();
+//        userDTO.id=user.getId();
+//        userDTO.kakaoNickName=user.getKakaoNickName();
+//        userDTO.serviceNickName=user.getServiceNickName();
+//        userDTO.email=user.getEmail();
+//        userDTO.age=user.getAge();
+//        userDTO.gender=user.getGender();
+//        userDTO.report_count=user.getReport_count();
+//        userDTO.review_count=user.getReview_count();
+//        userDTO.grade=user.getGrade();
+//        userDTO.blacklist_status=user.isBlacklist_status();
+//        userDTO.access_Token=user.getAccess_Token();
+//        userDTO.refresh_Token=user.getRefresh_Token();
+//        return userDTO;
+//
+//    }
 }
