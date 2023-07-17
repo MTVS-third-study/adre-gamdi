f6de9b19f536c151d166fa9b3a23183745cb21b1
manje cho <whakswp123@naver.com>
2023-07-17 14:22:44 +0900

    feat: 회원탈퇴 기능, 페이지 추가 (#54)

--- src/main/java/com/ohgiraffers/adregamdi/user/command/application/controller/UserController.java
+++ src/main/java/com/ohgiraffers/adregamdi/user/command/application/controller/UserController.java
@@ -1,16 +1,43 @@
 package com.ohgiraffers.adregamdi.user.command.application.controller;
 
+import com.ohgiraffers.adregamdi.user.command.application.dto.UserDTO;
 import com.ohgiraffers.adregamdi.user.command.application.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
     private final UserService userService;
 
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping("deleteForm")
+    public String goDeleteUser() {
+        return "user/deleteForm";
+    }
+
+    @PostMapping("deleteUser")
+    public String deleteUser(HttpSession session, ModelAndView mv) {
+        Long result = userService.deleteUser(((UserDTO) session.getAttribute("loginUser")).getKakaoId());
+        System.out.println("result = " + result);
+        session.removeAttribute("loginUser");
+        session.removeAttribute("service");
+        return "redirect:/";
+    }
+
+    @PostMapping("deleteNo")
+    public String deleteNo() {
+        return "redirect:/myPage";
     }
 
 }
