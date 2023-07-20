4f50e7274398040ba3e028fbbc9e67724f1184da
anichan <qufekf97@gmail.com>
2023-07-20 18:44:45 +0900

    서비스 이름 변경

--- src/main/java/com/ohgiraffers/adregamdi/place/query/application/service/PlaceQueryService.java
+++ src/main/java/com/ohgiraffers/adregamdi/place/query/application/service/PlaceQueryService.java
@@ -1,26 +1,23 @@
 package com.ohgiraffers.adregamdi.place.query.application.service;
 
+import com.ohgiraffers.adregamdi.place.command.domain.aggregate.entity.Place;
+import com.ohgiraffers.adregamdi.place.query.application.dto.PlaceDTO;
 import com.ohgiraffers.adregamdi.place.query.domain.entity.SearchPlace;
 import com.ohgiraffers.adregamdi.place.query.infra.repository.PlaceMapper;
-import org.apache.ibatis.session.SqlSession;
-import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 @Service
-public class PlaceService {
+public class PlaceQueryService {
 
     private final PlaceMapper placeMapper;
 
     @Autowired
-    public PlaceService(PlaceMapper placeMapper){
+    public PlaceQueryService(PlaceMapper placeMapper){
         this.placeMapper = placeMapper;
     }
 
@@ -28,6 +25,7 @@ public class PlaceService {
         List<SearchPlace> placeList = placeMapper.searchPlace(keyword);
         return placeList;
     }
+
 
 
 }
