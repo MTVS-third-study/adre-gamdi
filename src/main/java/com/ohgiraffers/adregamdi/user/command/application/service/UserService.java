3fcfd11ec57bcb3aae7aaf91980031842c1fae94
manje cho <whakswp123@naver.com>
2023-07-21 03:29:45 +0900

    feat: 회원 탈퇴 기능 로직 수정, 주석 추가 (#107)

--- src/main/java/com/ohgiraffers/adregamdi/user/command/application/service/UserService.java
+++ src/main/java/com/ohgiraffers/adregamdi/user/command/application/service/UserService.java
@@ -2,6 +2,7 @@ package com.ohgiraffers.adregamdi.user.command.application.service;
 
 import com.ohgiraffers.adregamdi.user.command.application.dto.UserDTO;
 import com.ohgiraffers.adregamdi.user.command.domain.repository.UserRepository;
+import com.ohgiraffers.adregamdi.user.command.domain.service.KakaoDomainService;
 import com.ohgiraffers.adregamdi.user.command.domain.service.UserDomainService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,11 +11,13 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository userRepository;
     private final UserDomainService userDomainService;
+    private final KakaoDomainService kakaoDomainService;
 
     @Autowired
-    public UserService(UserRepository userRepository, UserDomainService userDomainService) {
+    public UserService(UserRepository userRepository, UserDomainService userDomainService, KakaoDomainService kakaoDomainService) {
         this.userRepository = userRepository;
         this.userDomainService = userDomainService;
+        this.kakaoDomainService = kakaoDomainService;
     }
 
     // 닉네임 변경
@@ -24,7 +27,13 @@ public class UserService {
     }
 
     // 회원 탈퇴
-    public Long deleteUser(String id) {
-        return userRepository.deleteByKakaoId(id);
+    public int deleteUser(String token) {
+        Long kakaoId = kakaoDomainService.unlinkKakao(token); // 카카오 연결끊기
+
+        if (kakaoId != 0L) { // 카카오 연결끊기 성공 시
+            userRepository.deleteByKakaoId(String.valueOf(kakaoId));
+            return 1;
+        }
+        return 0; // 실패 시
     }
 }
