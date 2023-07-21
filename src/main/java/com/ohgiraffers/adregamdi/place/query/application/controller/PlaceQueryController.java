ed53d5f297d5a2267728f5d0241f849feb8561ff
anichan <qufekf97@gmail.com>
2023-07-21 19:22:27 +0900

    검색결과 없을 때, 검색 결과가 없습니다 출력

--- src/main/java/com/ohgiraffers/adregamdi/place/query/application/controller/PlaceQueryController.java
+++ src/main/java/com/ohgiraffers/adregamdi/place/query/application/controller/PlaceQueryController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -23,10 +24,17 @@ public class PlaceQueryController {
     }
 
     @GetMapping("/searchPlace")
-    public String searchPlace(@RequestParam("searchKeyword") String keyword, Model model){
-        List<PlaceDTO> placeList = placeQueryService.findPlaceByKeyword(keyword);
-        model.addAttribute("placeList", placeList);
+    public String searchPlace(@RequestParam("searchKeyword") String keyword, Model model, ModelAndView mv){
 
+
+        List<PlaceDTO> placeList = placeQueryService.findPlaceByKeyword(keyword);
+        boolean decide = true;
+
+        if (placeList.size() == 0) {
+            model.addAttribute("decide",decide);
+        }else {
+            model.addAttribute("placeList", placeList);
+        }
         return "schedule";
     }
 }
