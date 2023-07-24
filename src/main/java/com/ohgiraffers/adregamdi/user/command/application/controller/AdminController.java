0b9b7463109d28e518b1605adf505c6bca8aaf50
manje cho <whakswp123@naver.com>
2023-07-24 15:10:05 +0900

    feat: 회원 등급 변경 추가 (#142)

--- src/main/java/com/ohgiraffers/adregamdi/user/command/application/controller/AdminController.java
+++ src/main/java/com/ohgiraffers/adregamdi/user/command/application/controller/AdminController.java
@@ -1,8 +1,32 @@
 package com.ohgiraffers.adregamdi.user.command.application.controller;
 
-import org.springframework.stereotype.Controller;
+import com.ohgiraffers.adregamdi.user.command.application.dto.UserDTO;
+import com.ohgiraffers.adregamdi.user.command.application.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("/admin")
 public class AdminController {
+    private final AdminService adminService;
+
+    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    // 회원 등급 변경
+    @PostMapping("updateGrade")
+    public void updateGrade(@RequestParam("grade") int grade, HttpServletResponse response, HttpSession session) throws Exception {
+        UserDTO loginUser = (UserDTO) session.getAttribute("loginUser");
+        session.setAttribute("loginUser", adminService.updateGrade(grade, loginUser));
+        response.sendRedirect("http://localhost:8080/adminPage"); // 메인 페이지로
+    }
 
 }
