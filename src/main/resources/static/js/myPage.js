90f7f51f7e6ffdc280389bad1e877d2cbb155a15
JM <maxsdf@naver.com>
2023-07-30 18:36:00 +0900

    placeReqPage 추가

--- src/main/resources/static/js/myPage.js
+++ src/main/resources/static/js/myPage.js
@@ -1,8 +1,7 @@
 let modifyBtn = document.getElementById("modifyBtn");
 let deleteBtn = document.getElementById("deleteBtn");
-
-
-
+let contentsWrap=document.getElementsByClassName("contentsWrap")
+let placeReqWrap=document.getElementsByClassName("placeReqWrap")
 let menuListItem=document.querySelectorAll("#menuList>div");
 for(let i=0;i<menuListItem.length;i++){
     console.log(menuListItem[i]);
@@ -12,6 +11,14 @@ for(let i=0;i<menuListItem.length;i++){
     menuListItem[i].addEventListener("mouseleave",()=>{
         menuListItem[i].classList.remove("on")
     }  )
+    menuListItem[0].addEventListener("click",()=>{
+        contentsWrap[0].style.display="block";
+        placeReqWrap[0].style.display="none";
+    })
+    menuListItem[1].addEventListener("click",()=>{
+        contentsWrap[0].style.display="none";
+        placeReqWrap[0].style.display="block";
+    })
 }
 
 
