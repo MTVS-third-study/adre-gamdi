5957ced57f0de7cfe3c53a43437b1a12f434b295
wanny <kko6369@naver.com>
2023-07-31 10:12:41 +0900

    feat : 내 일정 불러오기, 삭제 구현

---
+++ src/main/java/com/ohgiraffers/adregamdi/schedule/command/application/dto/DeleteScheduleDTO.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.adregamdi.schedule.command.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeleteScheduleDTO {
+
+    private String message;
+}
